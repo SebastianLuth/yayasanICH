741c3eaad4bd6a72f16113eb090fcd0b8f3c3cb0
Sebastian Luth Hasibuan <134922313+SebastianLuth@users.noreply.github.com>
2024-07-10 01:53:28 +0700

    Update DetailConsultation.vue

--- src/views/productPage/DetailConsultation.vue
+++ src/views/productPage/DetailConsultation.vue
@@ -24,7 +24,7 @@
             </ul>
         </div>
         <div class="text-center mt-4">
-            <a class="cta-button" style="width: 500px; border-radius: 10px;" href="https://wa.me/0813-6168-8917">Temukan Solusi Bisnismu &raquo;</a>
+            <a class="cta-button" style="width: 500px; border-radius: 10px;" href="https://wa.me/+6281361688917">Temukan Solusi Bisnismu &raquo;</a>
         </div>
     </div>
     <NotFound v-else />
