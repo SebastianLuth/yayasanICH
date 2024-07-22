6fad1b41d937dffbdf76047f02f5a2b947777dbf
Sebastian Luth Hasibuan <134922313+SebastianLuth@users.noreply.github.com>
2024-07-22 08:15:10 +0700

    Update ModuleProduct.vue

--- src/views/admin/ModuleProduct.vue
+++ src/views/admin/ModuleProduct.vue
@@ -1,24 +1,90 @@
 <template>
-  <h1 style="text-align: start" class="mb-5">Semua Module</h1>
-  <div v-if="viewsModules.length > 0" class="row">
-    <div v-for="module in viewsModules" :key="module.id" class="col-md-4 mb-4">
-      <div class="card">
-        <div class="card-header d-flex align-items-center border-bottom">
-          <span class="avatar text-bg-primary avatar-lg fs-5">R</span>
-          <div class="ms-3">
-            <h6 class="mb-0 fs-sm">{{ module.judul }}</h6>
-            <span class="text-muted fs-sm"
-              >{{ module.penulis }},
-              {{ formatDate(module.tanggal_dibuat) }}</span
+  <div class="shadow-lg p-3 mb-5 bg-body-tertiary rounded">
+    <h1 style="text-align: start" class="mb-2 mt-3">Semua Module</h1>
+    <button class="btn btn-primary mb-3">
+        <router-link :to="{ name: 'post-module' }" id="about-link" class="text-white text-decoration-none">
+            Tambahkan Modul
+        </router-link>
+    </button>
+    <div v-if="viewsModules.length > 0" class="row">
+      <div
+        v-for="module in viewsModules"
+        :key="module.id"
+        class="col-md-4 mb-4"
+      >
+        <div class="card">
+          <div class="card-header d-flex align-items-center border-bottom">
+            <span class="avatar text-bg-primary avatar-lg fs-5">R</span>
+            <div class="ms-3">
+              <h6 class="mb-0 fs-sm">{{ module.judul }}</h6>
+              <span class="text-muted fs-sm"
+                >{{ module.penulis }},
+                {{ formatDate(module.tanggal_dibuat) }}</span
+              >
+            </div>
+            <button
+              class="btn text-muted ms-auto"
+              type="btn"
+              @click="confirmRemoveModul(module.id)"
             >
+              <i class="fas fa-times"></i>
+            </button>
+            <button
+              class="btn text-primary ms-2"
+              @click="editModule(module.id)"
+            >
+              Edit
+            </button>
           </div>
-          <button class="btn text-muted ms-auto" type="btn" @click="confirmRemoveModul(module.id)">
-            <i class="fas fa-times"></i>
-          </button>
+          <div class="card-body" style="height: 250px">
+            <h6>Module ID: {{ module.id }}</h6>
+            <p>{{ truncatedDescription(module.deskripsi) }}</p>
+          </div>
         </div>
-        <div class="card-body" style="height: 250px">
-          <h6>Module ID: {{ module.id}}</h6>
-          <p>{{ module.deskripsi }}</p>
+      </div>
+    </div>
+  </div>
+
+  <div class="shadow-lg p-3 mb-5 bg-body-tertiary rounded">
+    <h1 style="text-align: start" class="mb-2 mt-3">Semua Product</h1>
+    <button class="btn btn-primary mb-3">
+        <router-link :to="{ name: 'post-product' }" id="about-link" class="text-white text-decoration-none">
+            Tambahkan Produk
+        </router-link>
+    </button>
+    <div v-if="viewsProducts.length > 0" class="row">
+      <div
+        v-for="product in viewsProducts"
+        :key="product.code"
+        class="col-md-4 mb-4"
+      >
+        <div class="card">
+          <div class="card-header d-flex align-items-center border-bottom">
+            <span class="avatar text-bg-primary avatar-lg fs-5">R</span>
+            <div class="ms-3">
+              <h6 class="mb-0 fs-sm">{{ product.name }}</h6>
+              <span class="text-muted fs-sm"
+                >Target Audience : {{ product.targetAudience[0] }},
+              </span>
+            </div>
+            <button
+              class="btn text-muted ms-auto"
+              type="btn"
+              @click="confirmRemoveProduct(product.id)"
+            >
+              <i class="fas fa-times"></i>
+            </button>
+            <button
+              class="btn text-primary ms-2"
+              @click="editProduct(product.id)"
+            >
+              Edit
+            </button>
+          </div>
+          <div class="card-body" style="height: 250px">
+            <p>{{ product.description }}</p>
+            <p>Module ID yang digunakan {{ product.modules[0] }}</p>
+          </div>
         </div>
       </div>
     </div>
@@ -28,6 +94,7 @@
       <script>
 import { useAuthStore } from "../../stores/auth";
 import { onMounted, ref } from "vue";
+import { useRouter } from "vue-router";
 
 export default {
   components: {},
@@ -35,33 +102,67 @@ export default {
     const authStore = useAuthStore();
     const user = ref(authStore.userDetail);
     const viewsModules = ref([]);
+    const viewsProducts = ref([]);
+    const router = useRouter();
 
-
-    const fetchgetViewsAllModules = async () => {
+    const fetchgetViewsAll = async () => {
       const data = await authStore.viwesAllModules();
-      viewsModules.value = data;
+      const modules = data.modules;
+      // console.log(modules)
+      const products = data.products;
+      // console.log(products)
+      viewsModules.value = modules;
+      viewsProducts.value = products;
+    };
+
+    const editModule = (moduleId) => {
+      router.push(`/admin/module/${moduleId}`);
+    };
+    const editProduct = (productId) => {
+      router.push(`/admin/product/${productId}`);
     };
 
     const removeModule = async (modulId) => {
       try {
-        console.log(modulId)
-        await authStore.removeModule(modulId)
+        // console.log(modulId)
+        await authStore.removeModule(modulId);
         alert("Modul berhasil dihapus.");
         location.reload();
       } catch (error) {
-        console.error("Failed to remove Module:", error.message);
+        // console.error("Failed to remove Module:", error.message);
       }
-    }
+    };
+    const removeProduct = async (productId) => {
+      try {
+        // console.log(productId)
+        await authStore.removeProduct(productId);
+        alert("Product berhasil dihapus.");
+        location.reload();
+      } catch (error) {
+        // console.error("Failed to remove Module:", error.message);
+      }
+    };
 
     const confirmRemoveModul = async (modulId) => {
-      if (confirm("Apakah Anda yakin ingin menghapus pengguna ini?")) {
+      if (confirm("Apakah Anda yakin ingin menghapus modul ini?")) {
         removeModule(modulId);
       }
-    }
+    };
+    const confirmRemoveProduct = async (productId) => {
+      if (confirm("Apakah Anda yakin ingin menghapus product ini?")) {
+        removeProduct(productId);
+      }
+    };
 
     const formatDate = (dateString) => {
       const options = { year: "numeric", month: "long", day: "numeric" };
       return new Date(dateString).toLocaleDateString("id-ID", options);
+    };
+
+    const truncatedDescription = (description) => {
+      return description.length > 100
+        ? description.substring(0, 200) + "......"
+        : description;
     };
 
     onMounted(async () => {
@@ -69,12 +170,20 @@ export default {
         this.$router.replace({ name: "login" }); // redirect to login page
       }
 
-
-      await fetchgetViewsAllModules();
-      // console.log(fetchgetViewsAllModules());
+      await fetchgetViewsAll();
     });
 
-    return { user, viewsModules, formatDate,confirmRemoveModul };
+    return {
+      user,
+      editModule,
+      editProduct,
+      viewsModules,
+      formatDate,
+      confirmRemoveModul,
+      confirmRemoveProduct,
+      truncatedDescription,
+      viewsProducts,
+    };
   },
 };
 </script>
